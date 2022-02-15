d6321acf3b7a95d56832dc0835085ca354888894
redDwarf03 <rddwarf03@gmail.com>
2022-02-15 17:40:46 +0100

    add license infos

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,18 @@
+///   Ledger Dart Library
+///   (c) 2022 redDwarf03
+///
+///  Licensed under the GNU Affero General Public License, Version 3 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+//
+///      https://www.gnu.org/licenses/agpl-3.0.en.html
+//
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
